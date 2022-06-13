a15facb90e15a29a3a39eacf1e3316d4820f23eb
xiaozhicn <843369250@qq.com>
2022-06-13 17:45:06 +0800

    Update main.go

--- muke-go/bobby-resources-master/bobby-resources/go源码/mxshop_srvs/order_srv/rocketmq_test/consumer/main.go
+++ muke-go/bobby-resources-master/bobby-resources/go源码/mxshop_srvs/order_srv/rocketmq_test/consumer/main.go
@@ -11,16 +11,16 @@ import (
 )
 
 func main() {
-	c, _ := rocketmq.NewPushConsumer(
+	c, _ := rocketmq.NewPushConsumer(  //服务器主动推过来
 		consumer.WithNameServer([]string{"192.168.0.104:9876"}),
-		consumer.WithGroupName("mxshop"),
+		consumer.WithGroupName("mxshop"),  //不同的消费者，相同的groupname能避免重复消费
 	)
-
+        //订阅 topic  imooc1  下的数据，这个方法是放在一个协程中去做的，所以要sleep主协程
 	if err := c.Subscribe("imooc1", consumer.MessageSelector{}, func(ctx context.Context, msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
 		for i := range msgs {
 			fmt.Printf("获取到值： %v \n", msgs[i])
 		}
-		return consumer.ConsumeSuccess, nil
+		return consumer.ConsumeSuccess, nil //返回consumesuccess 这个值就不会再被取到，consumeretryLate 会再次取到
 	}); err != nil {
 		fmt.Println("读取消息失败")
 	}
